df46911dd6aa7f0ac73c39b52a9b77f570cbc891
khaitbek <khaitbek2005@gmail.com>
2022-10-21 11:26:40 +0500

    update adding functionality

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-const todoTemplate = document.querySelector("[data-todo]").content.cloneNode(true).children[0]
 const todoList = document.querySelector("#todoList")
 const todoForm = document.querySelector("#todoForm")
 const userTodoInput = document.querySelector("#userTodoInput")
-
+const todos = []
+const savedTodos = []
 todoForm.addEventListener("submit",(evt)=>{
     evt.preventDefault()
     const userTodoText = userTodoInput.value
+    addTodo(userTodoText)
+    todoForm.reset()
+})
+
+function addTodo(todo){
+    const todoTemplate = document.querySelector("[data-todo]").content.cloneNode(true).children[0]
     const todoTitle = todoTemplate.querySelector("#todoTitle")
-    todoTitle.textContent = userTodoText
+    todoTitle.textContent = todo
+    todos.push({
+        title:todo,
+        completed:false
+    })
     todoList.append(todoTemplate)
+}
+
+todoList.addEventListener("click",(evt)=>{
+    if(evt.target.matches("#saveTodo")){
+        
+    }
 })
